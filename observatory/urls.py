7e4f505845c8a06c3c814da4314db3d535a1a519
Nate Stedman <natesm@gmail.com>
2010-11-14 21:20:40 -0500

    A bit of CSS/layout.

--- observatory/urls.py
+++ observatory/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -37,6 +38,10 @@ urlpatterns = patterns('',
     (r'^projects/update/(\d+)', 'dashboard.views.projects.update'),
     (r'^projects/(\d+)', 'dashboard.views.projects.show'),
     (r'^projects/list', 'dashboard.views.projects.list'),
+    
+    # serve media (for now)
+    (r'^site-media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
     
     # default to showing the dashboard view (for now)
     (r'', 'dashboard.views.projects.index')
