09a0e987a916910bfa29959be9f13217bcef5d8c
Colin Sullivan <colinsul@gmail.com>
2011-01-07 16:47:09 -0500

    Moved all form fields when adding project to sane partial templates.

--- observatory/dashboard/templatetags/form_tags.py
+++ observatory/dashboard/templatetags/form_tags.py
@@ -3,28 +3,86 @@ from django.template.loader import render_to_string
 
 register = template.Library()
 
-
 ###
-#   An inclusion tag for an input field on the UI.
+# A function that is used sort of like inherited from but not really to do stuff
+# for the fields of a form.  This stuff needs to happen on each field.
 #
-#   @param  {Object}  data  - The POST data for the form, keys are 'name' attributes
-#     of input elements.
+# @param  {String}  label  -  The label for this field (to display to the user)
+# @param  {String}  id    - The id of this field element
+# @param  {String}  name  - The name of this field for the POST data
+# @param  {Object}  form  - The Django form object so the field can see errors and data
+# @param  {Anything/None}  required  - If required, this field is required
 ###
-@register.inclusion_tag('partials/input_field.html')
-def input_field(label, id, input_type, name, maxlength, data, required, placeholder=None):
-  
-  if name in data:
-    value = data[name]
+def field(label, id, name, form, required):
+  # If we have a default value
+  if name in form.data:
+    value = form.data[name]
   else:
     value = None
   
+  # IF we have errors on this field
+  if name in form.errors:
+    errors = form.errors[name]
+  else:
+    errors = None
+    
   return {
     'label': label, 
-    'id': id,
-    'input_type': input_type,  
-    'name': name,
-    'maxlength': maxlength, 
-    'value': value, 
+    'id': id, 
+    'name': name, 
     'required': required, 
-    'placeholder': placeholder, 
+    'value': value, 
+    'errors': errors, 
   }
+  
+
+###
+# This stuff needs to happen on each text field
+#
+# @param  {Number}  maxlength -  The max length attribute
+# @param  {String}  placeholder - The placeholder text for this field    
+###
+def text_field(label, id, name, form, required, maxlength, placeholder):
+  context = field(label, id, name, form, required)
+  context['maxlength'] = maxlength
+  context['placeholder'] = placeholder
+  
+  return context
+    
+###
+# The driver for an input field
+#
+# @param  {String}  input_type  - The type of input field
+###
+@register.inclusion_tag('partials/input_field.html')
+def input_field(label, id, name, form, required, maxlength, placeholder, input_type):
+  context = text_field(label, id, name, form, required, maxlength, placeholder)
+  context['input_type'] = input_type
+  
+  return context
+  
+###
+# A textarea field.
+#
+# @param  {Number}  rows  - rows attribute of <textarea> element
+# @param  {Number}  cols  - cols attribute of the <textarea> element
+###
+@register.inclusion_tag('partials/textarea_field.html')
+def textarea_field(label, id, name, form, required, maxlength, placeholder, rows, cols):
+  context = text_field(label, id, name, form, required, maxlength, placeholder)
+  context['rows'] = rows
+  context['cols'] = cols
+  
+  return context
+  
+###
+# A select field
+#
+# @param  {String}  default  -  The default value for the dropdown.
+###
+@register.inclusion_tag('partials/select_field.html')
+def select_field(label, id, name, form, required, default):
+  context = field(label, id, name, form, required)
+  context['default'] = default
+  context['options'] = form.fields[name].choices
+  return context
