eb97d8f8370e493f4170ace6b3eb2631173bfacd
Nate Stedman <natesm@gmail.com>
2011-01-18 11:15:20 -0500

    Add AUTHORS and chance © notice

--- observatory/dashboard/feeds.py
+++ observatory/dashboard/feeds.py
@@ -1,6 +1,6 @@
 # coding=utf8
 
-# Copyright (c) 2010, Nate Stedman <natesm@gmail.com>
+# Copyright (c) 2010, individual contributors (see AUTHORS file)
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
--- observatory/dashboard/fetch/parse_repository.py
+++ observatory/dashboard/fetch/parse_repository.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright (c) 2010, Nate Stedman <natesm@gmail.com>
+# Copyright (c) 2010, individual contributors (see AUTHORS file)
 #
 # Permission to use, copy, modify, and/or distribute this software for any
 # purpose with or without fee is hereby granted, provided that the above
