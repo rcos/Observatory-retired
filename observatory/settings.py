160eeb2925b78e862a5f2cb2cbde790f0306e49e
Nate Stedman <natesm@gmail.com>
2011-01-19 09:55:54 -0500

    Add RCOS views

--- observatory/settings.py
+++ observatory/settings.py
@@ -152,6 +152,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
+    'rcos',
 )
 
 # import recaptcha keys
