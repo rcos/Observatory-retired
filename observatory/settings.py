920d87e4f5bc5edf850d186725786d576d6ab084
Ben Shippee <ben.shippee@gmail.com>
2011-08-08 17:31:41 -0300

    Small whitespace change to test fetching scripts.

--- observatory/settings.py
+++ observatory/settings.py
@@ -34,6 +34,7 @@ LOGIN_URL='/login'
 # system time zone.
 TIME_ZONE = 'America/New_York'
 
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
