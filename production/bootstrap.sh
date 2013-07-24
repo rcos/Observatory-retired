ec4c35bd755cd6d0ab346fa01ea30505abf482ee
Colin Rice <colin@daedrum.net>
2013-07-24 13:55:22 -0400

    Fix run_puppet script and make bootstrap slightly cleaner

--- production/bootstrap.sh
+++ production/bootstrap.sh
@@ -3,8 +3,8 @@
 set -e
 
 ssh deploy@$1 "
-apt-get -y install git
+sudo apt-get -y install git
 git clone git://github.com/rcos/Observatory.git -b master --recursive
-~/Observatory/production/setup_puppet.sh
-~/Observatory/production/run_puppet.sh
+sudo ~/Observatory/production/setup_puppet.sh
+sudo ~/Observatory/production/run_puppet.sh
 "
--- production/run_puppet.sh
+++ production/run_puppet.sh
@@ -2,4 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd "$DIR/.."
+librarian-puppet install
+
 puppet apply  --modulepath "$DIR/../modules" $DIR/../puppet/base.pp --detailed-exitcodes || [ $? -eq 2 ]
