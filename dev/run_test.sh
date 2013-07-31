f838e8e3da37302cffaa9c1e6a21bf456f7c8e3b
Colin Rice <colin@daedrum.net>
2013-07-30 23:36:26 -0400

    Add rcos tests
    Add basic checks for all rcos pages asserting statuses of 200 and 301

--- dev/run_test.sh
+++ dev/run_test.sh
@@ -3,5 +3,5 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export PYTHONPATH="$DIR/../:$DIR/../observatory"
 export DJANGO_SETTINGS_MODULE="observatory.settings"
-py.test "$DIR/../observatory/tests"
+py.test "$DIR/../observatory"
 
