73992489794ef9a706e3c30a04683d7337609d5b
Colin Rice <colin@daedrum.net>
2013-08-06 15:10:50 -0400

    Add logging to send_mail

--- observatory/emaillist/methods.py
+++ observatory/emaillist/methods.py
@@ -8,6 +8,9 @@ def send_mail(subject, body, from_email, recipient_list, fail_silently=False):
     #Doing a separate email for each person so we can allow unsubscription links
     for addr in to:
 
+        print "Emailing: %s" % addr
+        print "Subject: %s" % subject
+
         #For now use default email body with an unsubscribe link
         html_content = '%s <br><a href="%s"> Unsubscribe From RCOS Emails</a>' % (body, reverse('emaillist.views.remove_email', args=[addr]))
 
