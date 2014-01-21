fab9c33ed2e4e8c7c43ecf548dbc49c7b8cfd752
Colin Rice <colin@daedrum.net>
2014-01-21 00:22:43 -0500

    Add parent directory in path by default

--- observatory/manage.py
+++ observatory/manage.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
 import os
+import os.path
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
+
+    #Include parent directory in the path by default
+    path = os.path.abspath('../')
+    if path not in sys.path:
+        sys.path.append(path)
+
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "observatory.settings")
 
     from django.core.management import execute_from_command_line
 
